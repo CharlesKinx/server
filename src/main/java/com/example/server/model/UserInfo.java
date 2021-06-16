0326aff4e8bdb102417ead4d8f0c926448e5093e
CharlesKinx <CharlesKinx@outlook.com>
2021-06-16 21:59:14 +0800

    第一次提交测试

---
+++ src/main/java/com/example/server/model/UserInfo.java
@@ -0,0 +1,15 @@
+package com.example.server.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserInfo {
+    private int id;
+    private String userAccount;
+    private String password;
+
+
+}
